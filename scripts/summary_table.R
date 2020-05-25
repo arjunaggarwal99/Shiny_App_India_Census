ba2d01875ae14c072ea9120fa82fe5258f90b842
Arjun Aggarwal <arjunaggarwal@Arjuns-MacBook-Pro.local>
2020-05-26 01:42:46 +0530

    Arjun table

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -1,16 +1,21 @@
-# This file calculates the summary table
+# This file calculates the summary table of Rural and Urban
+# households in India
 # The table is grouped by each State
 
 library(dplyr)
 
 # Extracting data and storing it in a variable
-census_df <- read.csv("data/india-districts-census-2011.csv")
+india_census_df <- read.csv("data/india-districts-census-2011.csv",
+                            stringsAsFactors = FALSE)
 
 # Creating a table grouped by state
-summary_table <- census_df %>%
+summary_table_households <- india_census_df %>%
   group_by(State.name) %>%
-  summarise("Total Population" = sum(Population),
-            "No. of rural househols" = sum(Rural_Households),
-            "No. of Urban Households" = sum(Urban_Households),
-            "Total number of households" = sum(Rural_Households) + sum(Urban_Households)
+  summarise(rural_households = sum(Rural_Households),
+            urban_households = sum(Urban_Households),
+            total_households = sum(Rural_Households) + sum(Urban_Households)
   )
+
+summary_table_households <- arrange(summary_table, rural_households,
+                                    urban_households,
+                                    total_households)
